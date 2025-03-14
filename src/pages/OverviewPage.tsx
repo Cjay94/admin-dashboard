c2de0bd7b17ff669669560e4e440028a3eabf858
Cjay94 <cjephthah94@gmail.com>
2025-03-14 11:28:55 +0200

    Created Stat Card Component

--- src/pages/OverviewPage.tsx
+++ src/pages/OverviewPage.tsx
@@ -1,9 +1,13 @@
+import { User } from "lucide-react"
 import Header from "../components/common/Header"
+import StatCard from "../components/common/StatCard"
 
 const OverviewPage = () => {
     return (
         <div>
             <Header title="Sample" />
+
+            <StatCard name="sample" icon={User} value="R1500" color="#000" />
         </div>
     )
 }
